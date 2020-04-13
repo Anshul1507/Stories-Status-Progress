21ea07f308b8fd7056a2d3c1af229efdb12dac08
Anshul1507 <ag1507anshul@gmail.com>
2020-04-13 19:39:43 +0530

    added click button functionalities

--- app/src/main/java/tech/dsckiet/discreteseekbar/MainActivity.java
+++ app/src/main/java/tech/dsckiet/discreteseekbar/MainActivity.java
@@ -4,14 +4,23 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
 import static tech.dsckiet.discreteseekbar.R.color.colorAccent;
+import static tech.dsckiet.discreteseekbar.R.color.colorPrimaryDark;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
-    int height,width,numberOfSections;
+    Button mButton;
+    int x=0,height,width,numberOfSections;
     LinearLayout layoutViews;
+    ArrayList<View> viewsList =new ArrayList<>();
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -21,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         //findviews
         layoutViews = findViewById(R.id.layout_views);
+        mButton = (Button) findViewById(R.id.button);
 
         //for getting dimensions of screen
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -30,16 +40,29 @@ public class MainActivity extends AppCompatActivity {
 
         //suppose we have to cut section into 4 parts
         numberOfSections = 10;
-        width -= (16 + 16*numberOfSections);
+        width -= (16 + 16*numberOfSections);    //reducing length of layout by 16dp from left and right and in between 16dp {in between*number of sections)
         width /= numberOfSections;
         for(int i=0;i<numberOfSections;i++){
             View v = new View(this);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, 10);
-            params.setMargins(16,0,0,0);
+            params.setMargins(16,0,0,0);    //giving 16dp internal margin between two views
             v.setLayoutParams(params);
+            viewsList.add(v);   //adding views in array list for changing color on click of button
 
             v.setBackgroundColor(getResources().getColor(colorAccent));
             layoutViews.addView(v);
         }
+        //button onclick function
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    buttonClick();
+            }
+        });
+    }
+
+    public void buttonClick(){
+       viewsList.get(x).setBackgroundColor(getResources().getColor(colorPrimaryDark));
+       x++;
     }
 }
